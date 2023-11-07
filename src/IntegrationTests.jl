c949f43d58a2881beb778a2811f494cc05c0a577
Simeon Ehrig <s.ehrig@hzdr.de>
2023-11-07 16:19:53 +0100

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.45

---
+++ src/IntegrationTests.jl
@@ -0,0 +1,5 @@
+module IntegrationTests
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using IntegrationTests
+using Test
+
+@testset "IntegrationTests.jl" begin
+    # Write your tests here.
+end
